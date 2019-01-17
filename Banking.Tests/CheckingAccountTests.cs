90fa73d6f80e75d04bb6e1b7b1ddc6184eabb31c
Joe Scheiman <scheiman.joe@gmail.com>
2019-01-17 15:10:00 -0500

    2nd commit

--- Banking.Tests/CheckingAccountTests.cs
+++ Banking.Tests/CheckingAccountTests.cs
@@ -3,12 +3,30 @@ using Xunit;
 
 namespace Banking.Tests
 {
-    public class UnitTest1
+    public class CheckingAccountTests
     {
         [Fact]
-        public void Test1()
+        public void Balance_Starts_At_Zero()
         {
+            // Act
+            CheckingAccount account = new CheckingAccount();
 
+            // Assert
+            Assert.Equal(0M, account.Balance);
+
+        }
+
+        [Fact]
+        public void Deposit_Increase_Balance()
+        {
+            // Arrange
+            CheckingAccount account = new CheckingAccount();
+
+            // Act
+            account.Deposit(200M);
+
+            // Assert
+            Assert.Equal(200M, account.Balance);
         }
     }
 }
